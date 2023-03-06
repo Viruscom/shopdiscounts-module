ab00abebff834209d8eeca923395f738665aeeb0
Viruscom <hristiqn_chobanov@abv.bg>
2023-03-06 14:03:53 +0200

    123123

--- Entities/Discount.php
+++ Entities/Discount.php
@@ -11,9 +11,9 @@ use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
 use Illuminate\Database\Eloquent\Relations\BelongsToMany;
 use Illuminate\Database\Eloquent\SoftDeletes;
-use Modules\Shop\Entities\Product;
 use Modules\Shop\Models\Admin\Brand;
 use Modules\Shop\Models\Admin\ProductCategory\Category;
+use Modules\Shop\Models\Admin\Products\Product;
 use Modules\ShopDiscounts\Database\factories\DiscountFactory;
 
 class Discount extends Model
