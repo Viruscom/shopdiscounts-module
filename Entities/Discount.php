b1a275cbad55e4cb9d7d116070fd323154dab428
Momchi Bachvarov <momchilbuchvarov@abv.bg>
2023-07-27 09:56:45 +0300

    moved methods to Product model

--- Entities/Discount.php
+++ Entities/Discount.php
@@ -362,11 +362,7 @@ class Discount extends Model
             }
         }
     }
-    public static function getQuantityDiscountAbstract($product)
-    {
-        return Discount::getBaseQuery()->where('type_id', Discount::$QUANTITY_TYPE_ID)
-            ->where('product_id', $product->id)->orderBy('created_at', 'desc')->first();
-    }
+   
     public static function getFreeDeliveryDiscount($basketProduct, $promoCode)
     {
         return Discount::getBaseQuery()->where('type_id', Discount::$FIXED_FREE_DELIVERY_TYPE_ID)
@@ -392,23 +388,6 @@ class Discount extends Model
             })->get()->first();
     }
 
-    public static function getFreeDeliveryDiscountAbstract($product)
-    {
-        return Discount::getBaseQuery()->where('type_id', Discount::$FIXED_FREE_DELIVERY_TYPE_ID)
-            ->where(function ($q) use ($product) {
-                return $q->where(function ($qq) {
-                    return $qq->where('applies_to', Discount::$EVERY_PRODUCT_APPLICATION);
-                })->orWhere(function ($qq) use ($product) {
-                    return $qq->where('applies_to', Discount::$PRODUCT_APPLICATION)->where('product_id', $product->id);
-                })->orWhere(function ($qq) use ($product) {
-                    return $qq->where('applies_to', Discount::$CATEGORY_APPLICATION)->whereHas('categories', function ($qqq) use ($product) {
-                        return $qqq->where('category_id', $product->category_id);
-                    });
-                })->orWhere(function ($qq) use ($product) {
-                    return $qq->where('applies_to', Discount::$BRAND_APPLICATION)->where('brand_id', $product->brand->id);
-                });
-            })->get()->first();
-    }
     public static function getFixedDiscounts($basketProduct, $promoCode)
     {
         $fixedDiscounts = Discount::getBaseQuery()->whereIn('type_id', [Discount::$FIXED_PERCENT_TYPE_ID, Discount::$FIXED_AMOUNT_TYPE_ID])
@@ -463,60 +442,16 @@ class Discount extends Model
         return $discounts;
     }
 
-    public static function getFixedDiscountsAbstract($product)
-    {
-        $fixedDiscounts = Discount::getBaseQuery()->whereIn('type_id', [Discount::$FIXED_PERCENT_TYPE_ID, Discount::$FIXED_AMOUNT_TYPE_ID])
-            ->where(function ($q) use ($product) {
-                return $q->where(function ($qq) {
-                    return $qq->where('applies_to', Discount::$EVERY_PRODUCT_APPLICATION);
-                })->orWhere(function ($qq) use ($product) {
-                    return $qq->where('applies_to', Discount::$PRODUCT_APPLICATION)->where('product_id', $product->id);
-                })->orWhere(function ($qq) use ($product) {
-                    return $qq->where('applies_to', Discount::$CATEGORY_APPLICATION)->whereHas('categories', function ($qqq) use ($product) {
-                        return $qqq->where('category_id', $product->category_id);
-                    });
-                })->orWhere(function ($qq) use ($product) {
-                    return $qq->where('applies_to', Discount::$BRAND_APPLICATION)->where('brand_id', $product->brand->id);
-                });
-            })->get();
-
-        $discounts        = [];
-        $currentAppliesTo = null;
-        foreach ($fixedDiscounts as $fixedDiscount) {
-            if (count($discounts) == 0) {
-                $discounts[$fixedDiscount->id] = $fixedDiscount;
-                $currentAppliesTo              = $fixedDiscount->applies_to;
-            } else {
-                if ($currentAppliesTo == Discount::$PRODUCT_APPLICATION) {
-                    if ($fixedDiscount->applies_to == Discount::$PRODUCT_APPLICATION) {
-                        $discounts[$fixedDiscount->id] = $fixedDiscount;
-                    }
-                } else {
-                    if ($currentAppliesTo == $fixedDiscount->applies_to) {
-                        $discounts[$fixedDiscount->id] = $fixedDiscount;
-                    } else if ($currentAppliesTo < $fixedDiscount->applies_to) {
-                        $discounts                     = [];
-                        $discounts[$fixedDiscount->id] = $fixedDiscount;
-                        $currentAppliesTo              = $fixedDiscount->applies_to;
-                    }
-                }
-            }
-        }
-
-        if (count($discounts) < 1) {
-            return null;
-        }
-
-        return $discounts;
-    }
     public static function getDiscountsAmount($discounts, $basketProductVatAppliedPrice)
     {
         $calcDiscountAmount = 0;
-        foreach ($discounts as $discountId => $discount) {
-            if ($discount->type_id == Discount::$FIXED_AMOUNT_TYPE_ID) {
-                $calcDiscountAmount += $discount->value;
-            } else {
-                $calcDiscountAmount += $basketProductVatAppliedPrice * ($discount->value / 100);
+        if(!is_null($discounts)) {
+            foreach ($discounts as $discountId => $discount) {
+                if ($discount->type_id == Discount::$FIXED_AMOUNT_TYPE_ID) {
+                    $calcDiscountAmount += $discount->value;
+                } else {
+                    $calcDiscountAmount += $basketProductVatAppliedPrice * ($discount->value / 100);
+                }
             }
         }
 
